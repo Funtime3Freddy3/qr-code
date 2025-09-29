73a3695faabaced02950423457a38fd21859adc2
Funtime3Freddy3 <42108357+Funtime3Freddy3@users.noreply.github.com>
2025-09-29 22:33:28 +0300

    Update app.php
    Wi-fi qr creation works now.

--- app.php
+++ app.php
@@ -10,7 +10,6 @@ use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
 if($_GET['download'] == '1') {
     $text = $_POST['text'] ?? 'Default QR';
-    //$size = isset($_POST['size']) ? intval($_POST['size']) : 1000;
     $qrCode = new QrCode($text);
     $writer = new PngWriter();
     $result = $writer->write($qrCode);
@@ -24,17 +23,13 @@ if($_GET['download'] == '1') {
     .option {
         display: none;
     }
-    option.selected {
+    .option.selected {
         display: block;
     }
 </style>
 <form>
     <h1 class="title">QR Code Generator</h1>
     <h3>Enter text or URL:</h3>
-    <!--
-    <p>Size:</p></br>
-    <p><input type="number" min="300" max="2000" id="qr-code-size" required>px</p></br>
-    -->
     <select name="mode" required>
         <option value="1">Text</option>
         <option value="2">URL</option>
@@ -50,30 +45,36 @@ if($_GET['download'] == '1') {
     </div>
     <div id="wifi-option" class="option">
         <h3>Encryption:</h3>
-        <input type="text" id="wifi-option-encryption" required></br>
+        <select id="wifi-option-encryption" required>
+            <option value="WPA" selected>WPA/WPA2</option>
+            <option value="WEP">WEP</option>
+            <option value="nopass">No password</option>
+        </select></br>
         <h3>SSID:</h3>
         <input type="text" id="wifi-option-ssid" required>
-        <h3>Password:</h3>
-        <input type="text" id="wifi-option-password" required>
+        <div id="wifi-option-password-block">
+            <h3>Password:</h3>
+            <input type="text" id="wifi-option-password" required>
+        </div>
     </div>
-    <button name="generate-qr-code" type="submit">Generate QR Code</button>
 </form>
 <script>
     const mode = document.querySelector('select[name=mode]');
     const modeText = document.querySelector('#text-option');
     const modeURL = document.querySelector('#url-option');
     const modeWifi = document.querySelector('#wifi-option');
-    //const QRCodeSize = document.querySelector('#qr-code-size');
     const QRCodeVar1 = document.querySelector('#text-option-text');
     const QRCodeVar2 = document.querySelector('#url-option-url');
     const QRCodeVar3 = document.querySelector('#wifi-option-encryption');
     const QRCodeVar4 = document.querySelector('#wifi-option-ssid');
     const QRCodeVar5 = document.querySelector('#wifi-option-password');
+    const QRCodeVar5Block = document.querySelector('#wifi-option-password-block');
     const generateQRCode = document.querySelector('button[name=generate-qr-code]');
     let memorizedMode = 1;
     modeText.classList.add('selected');
     modeURL.classList.remove('selected');
     modeWifi.classList.remove('selected');
+    QRCodeVar5Block.style.display = "block";
     mode.addEventListener('change', function() {
         modeType = parseInt(mode.value);
         memorizedMode = modeType;
@@ -81,6 +82,15 @@ if($_GET['download'] == '1') {
         modeURL.classList.toggle('selected', modeType === 2);
         modeWifi.classList.toggle('selected', modeType === 3);
     })
+    QRCodeVar3.addEventListener('change', function() {
+        if(QRCodeVar3.value === 'nopass') {
+            QRCodeVar5.required = false;
+            QRCodeVar5Block.style.display = "none";
+        } else {
+            QRCodeVar5.required = true;
+            QRCodeVar5Block.style.display = "block";
+        }
+    });
     generateQRCode.addEventListener('click', function() {
         let QRText = '';
         if(memorizedMode == 1) {
@@ -88,7 +98,11 @@ if($_GET['download'] == '1') {
         } else if(memorizedMode == 2) {
             QRText = QRCodeVar2.value;
         } else if(memorizedMode == 3) {
-            QRText = `WIFI:T:${QRCodeVar3.value};S:${QRCodeVar4.value};P:${QRCodeVar5.value};;`;
+            if(QRCodeVar3.value === 'nopass') {
+                QRText = `WIFI:T:${QRCodeVar3.value};S:${QRCodeVar4.value};;`;
+            } else {
+                QRText = `WIFI:T:${QRCodeVar3.value};S:${QRCodeVar4.value};P:${QRCodeVar5.value};;`;
+            }
         }
         fetch('app.php?download=1', {
             method: 'POST',
